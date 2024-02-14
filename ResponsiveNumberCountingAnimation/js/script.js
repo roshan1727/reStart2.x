339c3b7d9ca43d7dc0d7b21832c07f7bcd6ecb4b
roshan1727 <rakeshroshanp27@gmail.com>
2024-02-14 21:04:41 +0530

    counter animation

---
+++ ResponsiveNumberCountingAnimation/js/script.js
@@ -0,0 +1,19 @@
+//getting all the query selector
+let altervalues = document.querySelectorAll(".value");
+
+//displayng animated value via loops
+altervalues.forEach((singlevalue) => {
+    let startValue = 0;
+    let endValue = parseInt(singlevalue.getAttribute("data-value"));
+    let duration = Math.floor(2000 / endValue);
+
+    //counter for increaing the value &display
+    let counter = setInterval(() => {
+        startValue += 1;
+        singlevalue.textContent = startValue;
+
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+})
